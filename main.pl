5d91b8cc23869b0984a121a4b0003e33cd6053f3
Daniel Taskoff <daniel.taskoff@gmail.com>
2018-03-16 21:18:27 +0200

    Define de Bruijn terms and provide a to_de_bruijn predicate

--- main.pl
+++ main.pl
@@ -21,3 +21,23 @@ application(application(M, N)) --> lambda(M), [' '], term(N).
 application(application(M, N)) --> variable(M), [' '], term(N).
 application(application(M, N)) --> ['('], term(M), [')', ' '], term(N).
 variable(X) --> [C], { atom_string(X, C), v(X) }.
+
+% What is a 'de Bruijn' term?
+de_bruijn_term(X) :- number(X).
+de_bruijn_term(application(M, N)) :- de_bruijn_term(M), de_bruijn_term(N).
+de_bruijn_term(lambda(M)) :- de_bruijn_term(M).
+
+% Call the helper function with initialised accumulators
+to_de_bruijn(X, N) :- empty_assoc(G), to_de_bruijn(X, N, 0, 42, _, G, _).
+% Convert a lambda term into a de Bruijn term:
+%
+% convert a variable, which already has a de Bruijn index
+to_de_bruijn(X, N, L, K, K, G, G) :- v(X), get_assoc(X, G, Ni), N is Ni + L, !.
+% convert a variable, which doesn't have a de Bruijn index yet
+to_de_bruijn(X, N, L, K, Ki, G, Gi) :- v(X), N is K + L, put_assoc(X, G, K, Gi), Ki is K - 1.
+
+to_de_bruijn(application(M, N), applicaiton(MN, NN), L, K, Kii, G, Gii) :-
+  to_de_bruijn(M, MN, L, K, Ki, G, Gi), to_de_bruijn(N, NN, L, Ki, Kii, Gi, Gii).
+
+to_de_bruijn(lambda(X, M), lambda(MN), L, K, Ki, G, Giii) :-
+  put_assoc(X, G, -1 - L, Gi), to_de_bruijn(M, MN, L + 1, K, Ki, Gi, Gii), del_assoc(X, Gii, -1 - L, Giii).
