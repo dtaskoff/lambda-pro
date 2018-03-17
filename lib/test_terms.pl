bcb387f0c111e5c002af37b75f82a5800ecb14cf
Daniel Taskoff <daniel.taskoff@gmail.com>
2018-03-17 21:22:30 +0200

    Add some more terms to test_terms

--- lib/test_terms.pl
+++ lib/test_terms.pl
@@ -19,3 +19,18 @@ term(s, de_bruijn, atom, 'λ λ λ (2 0) (1 0)').
 term(s, de_bruijn, lambda, lambda(lambda(lambda(
   application(parentheses(application(2, 0)),
               parentheses(application(1, 0))))))).
+
+term(42, name, atom, 'x. fortytwo').
+term(42, name, lambda, lambda(x, fortytwo)).
+term(42, de_bruijn, atom, 'λ 43').
+term(42, de_bruijn, lambda, lambda(43)).
+
+term(c0, name, atom, 'f. x. x').
+term(c0, name, lambda, lambda(f, lambda(x, x))).
+term(c0, de_bruijn, atom, 'λ λ 0').
+term(c0, de_bruijn, lambda, lambda(lambda(0))).
+
+term(c1, name, atom, 'f. x. f x').
+term(c1, name, lambda, lambda(f, lambda(x, application(f, x)))).
+term(c1, de_bruijn, atom, 'λ λ 1 0').
+term(c1, de_bruijn, lambda, lambda(lambda(application(1, 0)))).
