a452232d715bded3f60e8c881a1a198eb0a64ffc
Daniel Taskoff <daniel.taskoff@gmail.com>
2018-03-17 10:59:15 +0200

    Use more descriptive names for the predicates

--- main.pl
+++ main.pl
@@ -1,32 +1,32 @@
 % What is a lambda term?
-term(X) :- v(X).                             % a variable
-term(application(M, N)) :- term(M), term(N). % an application
-term(lambda(X, M)) :- v(X), term(M).         % a lambda abstraction
-term(parentheses(T)) :- term(T).
+lterm(X) :- v(X).                             % a variable
+lterm(application(M, N)) :- term(M), term(N). % an application
+lterm(lambda(X, M)) :- v(X), term(M).         % a lambda abstraction
+lterm(parentheses(T)) :- term(T).
 
 :- use_module(utils, [atom_list_concat/2]).
 
-show_term(X, X) :- v(X).
-show_term(application(M, N), S) :- show_term(M, MS), show_term(N, NS),
+lterm_to_atom(X, X) :- v(X).
+lterm_to_atom(application(M, N), S) :- lterm_to_atom(M, MS), lterm_to_atom(N, NS),
   atom_list_concat([MS, ' ', NS], S).
-show_term(lambda(X, M), S) :- show_term(M, MS),
+lterm_to_atom(lambda(X, M), S) :- lterm_to_atom(M, MS),
   atom_list_concat([X, '. ', MS], S).
-show_term(parentheses(T), S) :- show_term(T, TS),
+lterm_to_atom(parentheses(T), S) :- lterm_to_atom(T, TS),
   atom_list_concat(['(', TS, ')'], S).
 
 % The set of allowed variables
 v(x). v(y). v(z). v(u). v(v). v(w).
 variables(V) :- bagof(X, v(X), V).
 
-% Lambda terms in a user-friendly format
-% term(x) -> x
-% term(application(M, N)) -> M N
-% term(lambda(x, M)) -> x. M
-% term(parentheses(T)) -> (T)
+% Convert a user-friendly lambda term into an internally represented lambda term
+% term ::= abstraction | application | variable | (term)
+% abstraction ::= variable. term
+% application ::= term term
+% variable ::= x | y | z | u | v | w
+atom_to_lterm(A, T) :- atom_chars(A, CS), parse(CS, T).
+string_to_lterm(S, T) :- string_chars(S, CS), parse(CS, T).
 
-% Parse a user-friendly lambda term
-% into a lambda term defined in prolog
-parse(S, T) :- string_chars(S, CS), once(phrase(term(T), CS)).
+parse(CS, T) :- once(phrase(term(T), CS)).
 
 term(T) --> lambda(T) | application(T) | variable(T) | parentheses(T).
 lambda(lambda(X, M)) --> variable(X), ['.', ' '], term(M).
@@ -37,52 +37,62 @@ variable(X) --> [C], { atom_string(X, C), v(X) }.
 parentheses(parentheses(T)) --> ['('], term(T), [')'].
 
 % What is a 'de Bruijn' term?
-de_bruijn_term(X) :- number(X).
-de_bruijn_term(application(M, N)) :- de_bruijn_term(M), de_bruijn_term(N).
-de_bruijn_term(lambda(M)) :- de_bruijn_term(M).
-de_bruijn_term(parentheses(T)) :- de_bruijn_term(T).
+dbterm(X) :- number(X).
+dbterm(application(M, N)) :- dbterm(M), dbterm(N).
+dbterm(lambda(M)) :- dbterm(M).
+dbterm(parentheses(T)) :- dbterm(T).
 
-show_de_bruijn_term(X, X) :- number(X).
-show_de_bruijn_term(application(M, N), S) :- show_de_bruijn_term(M, MS), show_de_bruijn_term(N, NS),
-  atom_list_concat([MS, ' ', NS], S).
-show_de_bruijn_term(lambda(M), S) :- show_de_bruijn_term(M, MS),
-  atom_list_concat(['λ ', MS], S).
-show_de_bruijn_term(parentheses(T), S) :- show_de_bruijn_term(T, TS),
-  atom_list_concat(['(', TS, ')'], S).
-
+dbterm_to_atom(X, X) :- number(X).
+dbterm_to_atom(application(M, N), S) :-
+  dbterm_to_atom(M, MS), dbterm_to_atom(N, NS), atom_list_concat([MS, ' ', NS], S).
+dbterm_to_atom(lambda(M), S) :-
+  dbterm_to_atom(M, MS), atom_list_concat(['λ ', MS], S).
+dbterm_to_atom(parentheses(T), S) :-
+  dbterm_to_atom(T, TS), atom_list_concat(['(', TS, ')'], S).
 
 % Call the helper function with initialised accumulators
-from_de_bruijn(N, X) :- variables(V), empty_assoc(G),
-  from_de_bruijn(N, X, 0, V, _, G, _).
+dbterm_to_lterm(N, X) :- variables(V), empty_assoc(G),
+  dbterm_to_lterm(N, X, 0, V, _, G, _).
 % Convert a de Bruijn term into a lambda term:
 %
 % convert a bound variable
-from_de_bruijn(N, X, L, V, V, G, G) :- number(N), Ni is N - L, get_assoc(Ni, G, X), !.
+dbterm_to_lterm(N, X, L, V, V, G, G) :-
+  number(N), Ni is N - L, get_assoc(Ni, G, X), !.
 % convert a free variable
-from_de_bruijn(N, X, L, [X|V], V, G, Gi) :- number(N), Ni is N - L, put_assoc(Ni, G, X, Gi).
+dbterm_to_lterm(N, X, L, [X|V], V, G, Gi) :-
+  number(N), Ni is N - L, put_assoc(Ni, G, X, Gi).
 
-from_de_bruijn(application(MN, NN), application(M, N), L, V, Vii, G, Gii) :-
-  from_de_bruijn(MN, M, L, V, Vi, G, Gi), from_de_bruijn(NN, N, L, Vi, Vii, Gi, Gii).
+dbterm_to_lterm(application(MN, NN), application(M, N), L, V, Vii, G, Gii) :-
+  dbterm_to_lterm(MN, M, L, V, Vi, G, Gi),
+  dbterm_to_lterm(NN, N, L, Vi, Vii, Gi, Gii).
 
-from_de_bruijn(lambda(MN), lambda(X, M), L, V, Vii, G, Giii) :-
-  from_de_bruijn(-1, X, L, V, Vi, G, Gi), Ni is -L-1, put_assoc(Ni, Gi, X, Gii), Li is L + 1, from_de_bruijn(MN, M, Li, Vi, Vii, Gii, Giii).
+dbterm_to_lterm(lambda(MN), lambda(X, M), L, V, Vii, G, Giii) :-
+  dbterm_to_lterm(-1, X, L, V, Vi, G, Gi),
+  Ni is -L-1, put_assoc(Ni, Gi, X, Gii), Li is L + 1,
+  dbterm_to_lterm(MN, M, Li, Vi, Vii, Gii, Giii).
 
-from_de_bruijn(parentheses(MN), parentheses(M), L, V, Vi, G, Gi) :-
-  from_de_bruijn(MN, M, L, V, Vi, G, Gi).
+dbterm_to_lterm(parentheses(MN), parentheses(M), L, V, Vi, G, Gi) :-
+  dbterm_to_lterm(MN, M, L, V, Vi, G, Gi).
 
 % Call the helper function with initialised accumulators
-to_de_bruijn(X, N) :- empty_assoc(G), to_de_bruijn(X, N, 0, 42, _, G, _).
+lterm_to_dbterm(X, N) :- empty_assoc(G),
+  lterm_to_dbterm(X, N, 0, 42, _, G, _).
 % Convert a lambda term into a de Bruijn term:
 %
 % convert a variable, which already has a de Bruijn index
-to_de_bruijn(X, N, L, K, K, G, G) :- v(X), get_assoc(X, G, Ni), N is Ni + L, !.
+lterm_to_dbterm(X, N, L, K, K, G, G) :-
+  v(X), get_assoc(X, G, Ni), N is Ni + L, !.
 % convert a variable, which doesn't have a de Bruijn index yet
-to_de_bruijn(X, N, L, K, Ki, G, Gi) :- v(X), N is K + L, put_assoc(X, G, K, Gi), Ki is K - 1.
+lterm_to_dbterm(X, N, L, K, Ki, G, Gi) :-
+  v(X), N is K + L, put_assoc(X, G, K, Gi), Ki is K - 1.
 
-to_de_bruijn(application(M, N), application(MN, NN), L, K, Kii, G, Gii) :-
-  to_de_bruijn(M, MN, L, K, Ki, G, Gi), to_de_bruijn(N, NN, L, Ki, Kii, Gi, Gii).
+lterm_to_dbterm(application(M, N), application(MN, NN), L, K, Kii, G, Gii) :-
+  lterm_to_dbterm(M, MN, L, K, Ki, G, Gi),
+  lterm_to_dbterm(N, NN, L, Ki, Kii, Gi, Gii).
 
-to_de_bruijn(lambda(X, M), lambda(MN), L, K, Ki, G, Giii) :-
-  put_assoc(X, G, -1 - L, Gi), to_de_bruijn(M, MN, L + 1, K, Ki, Gi, Gii), del_assoc(X, Gii, -1 - L, Giii).
-to_de_bruijn(parentheses(T), parentheses(TN), L, K, Ki, G, Gi) :-
-  to_de_bruijn(T, TN, L, K, Ki, G, Gi).
+lterm_to_dbterm(lambda(X, M), lambda(MN), L, K, Ki, G, Giii) :-
+  put_assoc(X, G, -1 - L, Gi),
+  lterm_to_dbterm(M, MN, L + 1, K, Ki, Gi, Gii),
+  del_assoc(X, Gii, -1 - L, Giii).
+lterm_to_dbterm(parentheses(T), parentheses(TN), L, K, Ki, G, Gi) :-
+  lterm_to_dbterm(T, TN, L, K, Ki, G, Gi).
