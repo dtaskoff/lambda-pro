64d8e4367d2a29f8d1df36c71b5a17da9250ef1e
Daniel Taskoff <daniel.taskoff@gmail.com>
2018-03-22 08:13:29 +0200

    Interpret church numerals

---
+++ lib/church_encoding.pl
@@ -0,0 +1,11 @@
+:- module(church_encoding, [numeral_to_atom/2]).
+
+:- use_module(interpret, [interpret/3]).
+:- use_module(reduction, [b_reducetr/2]).
+% Utilities for displaying Church encoded data
+
+
+numeral_to_atom(N, A) :-
+  b_reducetr(application(application(N, succ-1), zero-0), T),
+  list_to_assoc([succ-[X, SX]>>(SX is X + 1), zero-0], Ms),
+  once(interpret(T, Ms, A)).
--- load.pl
+++ load.pl
@@ -3,9 +3,10 @@
   , index_of/3, eq/2, free_variables/2
   , b_reduce/2, substitute/4, up/3
   , e_reduce/2, free_in/2
-  , interpret/3
+  , interpret/3, numeral_to_atom/2
   ]).
 
 :- use_module(lib/terms).
 :- use_module(lib/reduction).
 :- use_module(lib/interpret).
+:- use_module(lib/church_encoding).
