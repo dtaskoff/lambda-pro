cc889406ed1577d1c65a13332f333a904c84ba59
Daniel Taskoff <daniel.taskoff@gmail.com>
2019-08-15 18:10:51 +0300

    Refactor: add more comments and improve readability

--- lib/terms.pl
+++ lib/terms.pl
@@ -45,9 +45,10 @@ term_to_atom_(abs(_, M), A, de_bruijn) :-
 
 % This one has more arguments, because it has to fill the missing variables' names or indices
 % atom_to_term(Atom, Term, Type, VariablesIn, VariablesOut), where
-% Variables ::= (NextIndex, Names)
-atom_to_term(A, T, Ty, V, Vi) :- empty_assoc(B), atom_chars(A, As),
-  once(phrase(trm(Ti, Ty, (B, _, V, Vi, 0)), As)), unpar(Ti, T).
+% Variables ::= if Type is 'normal', then next index,
+% else (type is 'de_bruijn'), a list of variables' names to fill
+atom_to_term(A, T, Ty, Vin, Vout) :- empty_assoc(B), atom_chars(A, As),
+  once(phrase(trm(TP, Ty, (B, _, Vin, Vout, 0)), As)), unpar(TP, T).
 
 unpar(par(T), U) :- unpar(T, U).
 unpar(abs(X, M), abs(X, N)) :- unpar(M, N).
@@ -57,31 +58,41 @@ unpar(X-I, X-I).
 % A grammar for reading user-friendly λ-terms
 % term(Term, Type, State), where
 % State ::= (BoundNamesIn, BoundNamesOut, VariablesIn, VariablesOut, LambdasPassed)
+% a term
 trm(T, Ty, S) --> par(T, Ty, S) | lmd(T, Ty, S) | app(T, Ty, S) | vrb(T, Ty, S).
+% a term in parens
 par(par(T), Ty, S) --> ['('], trm(T, Ty, S), [')'].
-lmd(abs(X, M), normal, (B, B, I, Ii, L)) --> nam(X), ['.', ' '],
-  { J is -1 - L, Li is L + 1, put_assoc(X, B, J, Bi) },
-  trm(M, normal, (Bi, _, I, Ii, Li)).
-lmd(abs(N, M), de_bruijn, (B, B, [N|Ns], Nsi, L)) --> ['λ', ' '],
-  { J is -L, Li is L + 1, put_assoc(J, B, N, Bi) },
-  trm(M, de_bruijn, (Bi, _, Ns, Nsi, Li)).
-app(App, Ty, (B, Bii, V, Vii, L)) -->
-  fnc(M, Ty, (B, Bi, V, Vi, L)), [' '], trm(N, Ty, (Bi, Bii, Vi, Vii, L)),
+% a lambda term
+lmd(abs(X, M), normal, (B, B, Iin, Iout, L)) --> nam(X), ['.', ' '],
+  { J is -1 - L, L1 is L + 1, put_assoc(X, B, J, B1) },
+  trm(M, normal, (B1, _, Iin, Iout, L1)).
+lmd(abs(N, M), de_bruijn, (B, B, [N|Ns], Ns1, L)) --> ['λ', ' '],
+  { J is -L, L1 is L + 1, put_assoc(J, B, N, B1) },
+  trm(M, de_bruijn, (B1, _, Ns, Ns1, L1)).
+% an application
+app(App, Ty, (B, B2, V, V2, L)) -->
+  fnc(M, Ty, (B, B1, V, V1, L)), [' '], trm(N, Ty, (B1, B2, V1, V2, L)),
   { reorder_app(M, N, App) }.
+% a function (the left part of an application)
 fnc(M, Ty, S) --> par(M, Ty, S) | lmd(M, Ty, S) | vrb(M, Ty, S).
-vrb(X-J, normal, (B, Bi, I, Ii, L)) --> nam(X),
+% a variable
+vrb(X-J, normal, (B, B1, I, I1, L)) --> nam(X),
   { get_assoc(X, B, K) ->
-      J is K + L, Bi = B, Ii = I;
-      J is I + L, Ii is I - 1, put_assoc(X, B, J, Bi) }.
-vrb(X-J, de_bruijn, (B, Bi, Ns, Nsi, L)) --> idx(J),
-  { Ji is J - L + 1, get_assoc(Ji, B, Y) ->
-      X = Y, Bi = B, Nsi = Ns;
-      Ns = [N|Nsi], X = N, put_assoc(J, B, X, Bi) }.
+      J is K + L, B1 = B, I1 = I;
+      J is I + L, I1 is I - 1, put_assoc(X, B, J, B1) }.
+vrb(X-J, de_bruijn, (B, B1, Ns, Ns1, L)) --> idx(J),
+  { J1 is J - L + 1, get_assoc(J1, B, Y) ->
+      X = Y, B1 = B, Ns1 = Ns;
+      Ns = [N|Ns1], X = N, put_assoc(J, B, X, B1) }.
+% a variable name
 nam(X) --> sym(S), nam(Y), { atom_concat(S, Y, X) }.
 nam(S) --> sym(S).
+% a symbol (part of a variable name)
 sym(S) --> [S], { S \= 'λ', S \= ' ', S \= '(', S \= ')', S \= '.' }.
-idx(X) --> dgt(D), idx(Y), { atom_concat(D, Y, Xi), atom_number(Xi, X) }.
+% an index (of de Bruijn)
+idx(X) --> dgt(D), idx(Y), { atom_concat(D, Y, X1), atom_number(X1, X) }.
 idx(D) --> dgt(D).
+% a digit (part of an index)
 dgt(D) --> [C], { char_type(C, digit(D)) }.
 
 % Reorder applications to match their left associativity
@@ -94,6 +105,8 @@ reorder_app(M, app(N, 1, P), app(Q, I, R)) :- !, reorder_app(M, N, Q),
 reorder_app(M, N, app(M, 1, N)).
 
 % α-equivalence for internally represented λ-terms
+% Note: counting on the fact, that applications are normalised, and parens are removed (see unpar)
+% (e.g. f (f x) can be represented as 'app(f-_, 2, x-_)', and as 'app(f-_, 1, app(f-_, 1, x-_)')
 eq(_-I, _-I) :- !, number(I).
 eq(app(M, I, N), app(P, I, Q)) :- !, eq(M, P), eq(N, Q).
 eq(abs(_, M), abs(_, N)) :- eq(M, N).
